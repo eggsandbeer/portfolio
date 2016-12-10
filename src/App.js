23992209b1cddf977147c1db6ed728f0f9226297
Cameron <cameron.strandberg@gmail.com>
2016-12-10 15:54:57 -0800

    Added old data, images. The CUBE works.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Cubes.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div id="CubeContainer">
+        <div id="Cube">
+          <div className="face1"></div>
+          <div className="face2"></div>
+          <div className="face3"></div>
+          <div className="face4"></div>
+          <div className="face5"></div>
+          <div className="face6"></div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
