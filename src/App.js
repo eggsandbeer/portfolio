0965e0b923b65cd9adfdbafb89c4a4df6f7f9e17
Cameron <cameron.strandberg@gmail.com>
2016-12-10 16:42:41 -0800

    Basic top level container and background stuff.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import './Cubes.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <div id="CubeContainer">
-        <div id="Cube">
-          <div className="face1"></div>
-          <div className="face2"></div>
-          <div className="face3"></div>
-          <div className="face4"></div>
-          <div className="face5"></div>
-          <div className="face6"></div>
+      <div className="TopLevelContainer">
+        <div className="ContentContainer">
+          <div id="CubeContainer">
+            <div id="Cube">
+              <div className="face1"></div>
+              <div className="face2"></div>
+              <div className="face3"></div>
+              <div className="face4"></div>
+              <div className="face5"></div>
+              <div className="face6"></div>
+            </div>
           </div>
         </div>
       </div>
