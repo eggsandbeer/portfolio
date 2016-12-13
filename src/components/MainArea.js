3b493592b81cf59cc415d884da5ac98b4031efe2
Cameron <cameron.strandberg@gmail.com>
2016-12-13 13:33:18 -0800

    Happy with my transitions finally.

--- src/components/MainArea.js
+++ src/components/MainArea.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import ImageLoader from './ImageLoader';
 import './MainArea.css';
 
@@ -7,31 +6,25 @@ class MainArea extends Component {
   render(){
     return(
       <div className={"MainArea col-md-9"}>
-        <ReactCSSTransitionGroup
-          transitionName="switch"
-          transitionEnterTimeout={250}
-          transitionLeaveTimeout={250}
-        >
-          <div ref={(input) => { this.heightcontainer = input; }} key={this.props.set.id}>
-            <h3 key="h3">{this.props.set.title}</h3>
-            <p>
-              <a href={this.props.set.url}>
-                {this.props.set.urltitle}
-              </a>
-            </p>
-            <hr />
-            <p>
-              {this.props.set.content}
-            </p>
-            {this.props.set.images.map((set, i) =>
-              <ImageLoader
-                thumbnailimage={process.env.PUBLIC_URL+'/images/work/'+this.props.set.id+'.'+(i+1)+'.thumbnail.png'}
-                fullimage={process.env.PUBLIC_URL+'/images/work/'+this.props.set.id+'.'+(i+1)+'.png'}
-                ratio={set.ratio}
-              />
-            )}
-          </div>
-        </ReactCSSTransitionGroup>
+        <div ref={(input) => { this.heightcontainer = input; }} key={this.props.set.id}>
+          <h3 key="h3">{this.props.set.title}</h3>
+          <p>
+            <a href={this.props.set.url}>
+              {this.props.set.urltitle}
+            </a>
+          </p>
+          <hr />
+          <p>
+            {this.props.set.content}
+          </p>
+          {this.props.set.images.map((set, i) =>
+            <ImageLoader
+              thumbnailimage={process.env.PUBLIC_URL+'/images/work/'+this.props.set.id+'.'+(i+1)+'.thumbnail.png'}
+              fullimage={process.env.PUBLIC_URL+'/images/work/'+this.props.set.id+'.'+(i+1)+'.png'}
+              ratio={set.ratio}
+            />
+          )}
+        </div>
       </div>
     );
   }
